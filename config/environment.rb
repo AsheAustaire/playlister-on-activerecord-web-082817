4c9e46312e5d068019c180cc47a8644ccc400035
Ashe Austaire <AsheAustaire@gmail.com>
2017-09-15 10:39:44 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ Bundler.require
 require 'active_record'
 require 'rake'
 
+ActiveRecord::Base.logger = nil
+
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
@@ -19,7 +21,7 @@ def migrate_db
     DB.execute("DROP TABLE #{table}")
   end
 
-  Dir[File.join(File.dirname(__FILE__), "../db/migrate", "*.rb")].each do |f| 
+  Dir[File.join(File.dirname(__FILE__), "../db/migrate", "*.rb")].each do |f|
     require f
     migration = Kernel.const_get(f.split("/").last.split(".rb").first.gsub(/\d+/, "").split("_").collect{|w| w.strip.capitalize}.join())
     migration.migrate(:up)
